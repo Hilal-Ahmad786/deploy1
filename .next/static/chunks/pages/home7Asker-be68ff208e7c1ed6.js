14815b8a1f56d3bf2c9faa8c88a7fa8209e49f18
Hilal-Ahmad786 <hilalahmad.civilengineer@gmail.com>
2023-10-19 23:14:50 +0300

    first commit

---
+++ .next/static/chunks/pages/home7Asker-be68ff208e7c1ed6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[226],{8189:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home7Asker",function(){return u(8259)}])}},function(n){n.O(0,[151,259,774,888,179],(function(){return _=8189,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
